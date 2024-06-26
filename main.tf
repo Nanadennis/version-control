ed4501c9185722620c3b25c2406266fcd6185fc1
Nana Aba <nabdennis@gmail.com>
2024-06-26 20:14:27 +0000

    terraform first test

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.55.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+resource "aws_iam_user" "test" {
+  name = "nana"
+
+}
+
+
+
